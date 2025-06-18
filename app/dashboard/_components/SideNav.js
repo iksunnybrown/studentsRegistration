98f354a2486a98c9dcfe902d47a94d0f6ee7902a
iksunnybrown <iksunnybrown@gmail.com>
2025-06-18 11:58:28 +0100

    deploy: deployed the app

--- app/dashboard/_components/SideNav.js
+++ app/dashboard/_components/SideNav.js
@@ -1,9 +1,45 @@
+import { GraduationCap, Hand, LayoutIcon, Settings } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
 export default function SideNav() {
+  const menuList=[
+    {
+    id:1,
+    name: "Dashboard",
+    icon: LayoutIcon,
+    path:'/dashboard'
+  },
+    {
+    id:2,
+    name: "Students",
+    icon: GraduationCap,
+    path:'/dashboard/students'
+  },
+    {
+    id:3,
+    name: "Attendence",
+    icon: Hand,
+    path:'/dashboard/attendence'
+  },
+    {
+    id:4,
+    name: "Settings",
+    icon: Settings,
+    path:'/dashboard/settings'
+  },
+]
   return (
-    <div className='border shadow-md h-screen'>
-      SideNav
+    <div className='border-2 shadow-md h-screen p-5'>
+      <Image src={"/logo.svg"} width={180} height={50} alt='logo'/>
+
+      <hr className='my-5'></hr>
+      {menuList.map((menu, index)=>(
+        <h2>
+          <menu.icon />
+          {menu.name}
+        </h2>
+      ))}
     </div>
   )
 }
--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -3,13 +3,13 @@ import SideNav from "./_components/SideNav"
 
 export default function layout({children}) {
   return (
-    <>
+    <div>
       <div className='md:w-64 fixed hidden md:block'>
         <SideNav />
       </div>
       <div className='md:ml-64'>
       {children}
       </div>
-    </>
+    </div>
   )
 }
